bc6e4f3ba02e776e325ef4f07584c49364b99d6a
Lalit Max <lalitmay@gmail.com>
2024-08-30 23:52:09 +0530

    rr some fucn implement

--- roundRobin.c
+++ roundRobin.c
@@ -1,5 +1,58 @@
 #include <stdio.h>
 
+struct Process {
+    int pid;        // Process ID
+    int burst_time; // Burst Time
+    int remaining_time; // Remaining Time
+};
+
+ void roundRobin(struct Process proc[], int n, int quantum) {
+    int time = 0; 
+    int done = 0;  
+
+ 
+    while (done < n) {
+        for (int i = 0; i < n; i++) {
+            if (proc[i].remaining_time > 0) {
+                // If remaining time is greater than quantum
+                if (proc[i].remaining_time > quantum) {
+                    time += quantum;
+                    proc[i].remaining_time -= quantum;
+                }
+                // If remaining time is less than or equal to quantum
+                else {
+                    time += proc[i].remaining_time;
+                    proc[i].remaining_time = 0;
+                    done++;
+                    printf("Process %d finished at time %d\n", proc[i].pid, time);
+                }
+            }
+        }
+    }
+}
+
 int main() {
+    int n;  
+    int quantum;  
+
     
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    printf("Enter the time quantum: ");
+    scanf("%d", &quantum);
+
+    struct Process proc[n];
+
+     
+    for (int i = 0; i < n; i++) {
+        proc[i].pid = i + 1;
+        printf("Enter burst time for Process %d: ", proc[i].pid);
+        scanf("%d", &proc[i].burst_time);
+        proc[i].remaining_time = proc[i].burst_time;
+    }
+
+    
+    roundRobin(proc, n, quantum);
+
+    return 0;
 }
